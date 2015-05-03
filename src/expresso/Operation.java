77293e1ebbcfe9797f53a24d9399b8ec2e405675
Ashley Wang <ashwang@Ashleys-MacBook-Pro.local>
2015-05-03 12:23:05 -0400

    Specs in right place

--- src/expresso/Operation.java
+++ src/expresso/Operation.java
@@ -2,24 +2,12 @@ package expresso;
 
 public class Operation implements Expression {
     
-    /**
-     * Multiply together two valid expressions, without any simplifications
-     * @param e1 - first expression
-     * @param e2 - second expression
-     * @return Expression that represents e1 * e2
-     */
     @Override
     public Expression multiply(Expression e1, Expression e2) {
         // TODO Auto-generated method stub
         return null;
     }
 
-    /**
-     * Add together two valid expressions, without any simplifications
-     * @param e1 - first expression
-     * @param e2 - second expression
-     * @return Expression that represents e1 + e2
-     */
     @Override
     public Expression add(Expression e1, Expression e2) {
         // TODO Auto-generated method stub
