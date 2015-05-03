86ddec5de13ba0f5805fb4c3a0c5abb5b59e71a3
Chandani <chandani@mit.edu>
2015-05-03 12:33:46 -0400

    spec for Variable

--- src/expresso/Variable.java
+++ src/expresso/Variable.java
@@ -1,7 +1,15 @@
 package expresso;
 
+/**
+ * This class represents strings that are variables in the expression
+ */
 public class Variable implements Expression {
-
+    
+    private String var;
+    public Variable(String var) {
+        this.var = var;
+    }
+    
     @Override
     public Expression multiply() {
         // TODO Auto-generated method stub
