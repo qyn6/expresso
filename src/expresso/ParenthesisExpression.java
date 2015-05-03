f1c428591fb3825eb93c99564fdbfcfe597f7abf
Yini Qi <qyn@mit.edu>
2015-05-03 10:39:04 -0400

    fix import

--- src/expresso/ParenthesisExpression.java
+++ src/expresso/ParenthesisExpression.java
@@ -8,16 +8,14 @@ import org.antlr.v4.runtime.TokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
-import expression.ExpressionTest.ExpressionListenerPrintEverything;
 import expresso.parser.ExpressionLexer;
-import expresso.parser.ExpressionListener;
 import expresso.parser.ExpressionParser;
 
 public class ParenthesisExpression {
     
     public static void main(String[] args) {
        
-    CharStream stream = new ANTLRInputStream("()");
+    CharStream stream = new ANTLRInputStream("()(())((()()))");
     ExpressionLexer lexer = new ExpressionLexer(stream);
     TokenStream tokens = new CommonTokenStream(lexer);
     
@@ -27,9 +25,6 @@ public class ParenthesisExpression {
     System.err.println(tree.toStringTree(parser));
     ((RuleContext)tree).inspect(parser);
     
-    ParseTreeWalker walker = new ParseTreeWalker();
-    //ExpressionListener listener = new ExpressionListenerPrintEverything();
-    //walker.walk(listener, tree);
     }
     
 }
