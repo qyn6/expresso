66174f2479e221eddca70880c7e01b729466c56e
Ashley Wang <ashwang@Ashleys-MacBook-Pro.local>
2015-05-03 15:56:11 -0400

    correct specs

--- src/expresso/Operation.java
+++ src/expresso/Operation.java
@@ -12,26 +12,24 @@ public class Operation implements Expression {
     }
 
     /**
-     * Add together two valid expressions, without any simplifications
-     * @param e1 - first expression
-     * @param e2 - second expression
-     * @return Expression that represents e1 + e2
+     * Add this expression together with another valid expression, without any simplifications
+     * @param e1 - expression to add
+     * @return Expression that represents e + e1
      */
-    public Expression add(Expression e) {
+    public Expression add(Expression e1) {
         // TODO Auto-generated method stub
-        return null;
+        return e.add(e1);
     }    
     /**
-     * Multiply together two valid expressions, without any simplifications
+     * Multiply this expression together with another valid expression, without any simplifications
      *     Edge case: There must always be a '*' symbol between expressions
      *     Ex: (3 + x)(3 - x) is not valid, but (3 + x)*(3 - x) is.
-     * @param e1 - first expression
-     * @param e2 - second expression
-     * @return Expression that represents e1 * e2
+     * @param e1 - expression to multiply
+     * @return Expression that represents e * e1
      */
-    public Expression multiply(Expression e) {
+    public Expression multiply(Expression e1) {
         // TODO Auto-generated method stub
-        return null;
+        return e.add(e1);
     }
 
 }
