d63585f35548238b3e9819a381a093f9264376ba
Ashley Wang <ashwang@Ashleys-MacBook-Pro.local>
2015-05-03 12:42:29 -0400

    specs, for real this time

--- src/expresso/Operation.java
+++ src/expresso/Operation.java
@@ -1,17 +1,24 @@
 package expresso;
 
-/**
- * This class represents the operations on two expressions. 
- *
- */
 public class Operation implements Expression {
-
-    public Expression multiply(Expression e1, Expression e2) {
+    /**
+     * Multiply together two valid expressions, without any simplifications
+     * @param e1 - first expression
+     * @param e2 - second expression
+     * @return Expression that represents e1 * e2
+     */
+    public static Expression multiply(Expression e1, Expression e2) {
         // TODO Auto-generated method stub
         return null;
     }
 
-    public Expression add(Expression e1, Expression e2) {
+    /**
+     * Add together two valid expressions, without any simplifications
+     * @param e1 - first expression
+     * @param e2 - second expression
+     * @return Expression that represents e1 + e2
+     */
+    public static Expression add(Expression e1, Expression e2) {
         // TODO Auto-generated method stub
         return null;
     }
