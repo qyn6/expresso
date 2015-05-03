c7d0b9247e58bf38f0486efb96132d36a9f5cddf
Ashley Wang <ashwang@Ashleys-MacBook-Pro.local>
2015-05-03 15:53:00 -0400

    specs

--- src/expresso/Expression.java
+++ src/expresso/Expression.java
@@ -29,7 +29,7 @@ public interface Expression {
     }
     
     public Expression add(Expression e);
-    // TODO Instance methods
+    
     public Expression multiply(Expression e);
 
     
--- src/expresso/Operation.java
+++ src/expresso/Operation.java
@@ -10,16 +10,6 @@ public class Operation implements Expression {
     public Operation (Expression e) {
         this.e = e;
     }
-    /**
-     * Multiply together two valid expressions, without any simplifications
-     * @param e1 - first expression
-     * @param e2 - second expression
-     * @return Expression that represents e1 * e2
-     */
-    public Expression multiply(Expression e) {
-        // TODO Auto-generated method stub
-        return null;
-    }
 
     /**
      * Add together two valid expressions, without any simplifications
@@ -28,6 +18,18 @@ public class Operation implements Expression {
      * @return Expression that represents e1 + e2
      */
     public Expression add(Expression e) {
+        // TODO Auto-generated method stub
+        return null;
+    }    
+    /**
+     * Multiply together two valid expressions, without any simplifications
+     *     Edge case: There must always be a '*' symbol between expressions
+     *     Ex: (3 + x)(3 - x) is not valid, but (3 + x)*(3 - x) is.
+     * @param e1 - first expression
+     * @param e2 - second expression
+     * @return Expression that represents e1 * e2
+     */
+    public Expression multiply(Expression e) {
         // TODO Auto-generated method stub
         return null;
     }
